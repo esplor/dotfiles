481023693c8378f1a9f3ba4bd6fffd064e98d0e6
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2022-06-01 16:13:00 +0200

    Update compose script, prepare for not debian

--- update-docker-compose.sh
+++ update-docker-compose.sh
@@ -6,9 +6,12 @@
 # Current user
 DC_PATH="$HOME/.docker/cli-plugins"
 
+DISTRO=$(lsb_release --id -s)
+
+echo -e "\nDistribution:\t$DISTRO"
+
 if [ ! -d $DC_PATH ]; then
-    echo "*** \n"
-    echo $DC_PATH directory does not exist, creating
+    echo -e "\n$DC_PATH directory does not exist, creating..."
     mkdir -p $DC_PATH
     curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DC_PATH/docker-compose
     chmod +x $DC_PATH/docker-compose
@@ -19,18 +22,19 @@ cat << EOF
     $compose_version
     ---- ----------------- ----
 EOF
-
 else
     compose_old="$(docker compose version)"
-    echo $DC_PATH exists, continue download...
-    curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DC_PATH/docker-compose
+    echo -e "\n$DC_PATH exists, continue download...\n\n"
+    curl -sSL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DC_PATH/docker-compose
     chmod +x $DC_PATH/docker-compose
     compose_new="$(docker compose version)"
 cat << EOF
-    ---- Old version ----
-    $compose_old
-    ---- New version ----
-    $compose_new
-    ----
+
+---- Old version ----
+$compose_old
+---- New version ----
+$compose_new
+----
+
 EOF
 fi
