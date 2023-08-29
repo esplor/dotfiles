7b8c968571cbbae181fd6a36275eb894b50c79f7
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2023-08-29 17:55:47 +0200

    Add colorscheme

--- .config/nvim/lua/esplor/plugins.lua
+++ .config/nvim/lua/esplor/plugins.lua
@@ -27,6 +27,27 @@ require("lazy").setup({
           highlight = { enable = true },
           indent = { enable = true },  
         })
-    end
- }
+    end,
+  },
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
 })
