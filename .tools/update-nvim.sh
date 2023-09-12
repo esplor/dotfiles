ef72374d575ba7b0ecbbb781e90fc67b0f2321bc
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2023-09-12 21:32:06 +0000

    Autocreate /home/eslo/.apps/bin

--- .tools/update-nvim.sh
+++ .tools/update-nvim.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env sh
+#/usr/bin/env bash
 
-# TODO: if ~/.apps not exist.. create
-wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xvz -C ~/.apps --strip-components=1
-echo -e "remember to run 'export PATH=~.apps/bin:$PATH'"
+dir=$HOME/.apps
+
+if [ ! -d $dir ]; then
+echo "Directory $dir doesn't exist, creating.."
+mkdir -p $dir
+fi
+
+wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xvz -C $dir --strip-components=1
+echo "-----\n$dir/bin needs to be included in path, export PATH=$dir/bin:'$PATH'\n-----"
