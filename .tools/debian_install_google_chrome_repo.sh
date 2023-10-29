dc1c7fd87d00855f080aa32b23cb41d93d44f22a
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2023-10-29 19:12:16 +0100

    Update google chrome script

--- .tools/debian_install_google_chrome_repo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee - /etc/apt/trusted.gpg.d/google-key.pub
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/google-key.pub] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt update && sudo apt install google-chrome-stable
