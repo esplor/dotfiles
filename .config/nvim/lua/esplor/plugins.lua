42bcf91b6ad40c9fd69c0974f2cdac9fc36eb362
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2023-11-08 22:58:37 +0100

    Add comment plugin to neovim

--- .config/nvim/lua/esplor/plugins.lua
+++ .config/nvim/lua/esplor/plugins.lua
@@ -50,4 +50,12 @@ require("lazy").setup({
       },
     },
   },
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    },
+    lazy = false,
+  },
 })
