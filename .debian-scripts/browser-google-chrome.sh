f114203e9a4d1253bcce42625cdfa1cd950a2363
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-11-10 23:09:36 +0100

    chrome: Add hint for default xdg browser

--- .debian-scripts/browser-google-chrome.sh
+++ .debian-scripts/browser-google-chrome.sh
@@ -3,3 +3,5 @@
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee - /etc/apt/trusted.gpg.d/google-key.pub
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/google-key.pub] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update && sudo apt install -y google-chrome-stable
+
+echo xdg-settings set default-web-browser google-chrome.desktop
