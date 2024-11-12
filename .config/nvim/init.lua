d65fcb574035cbdb6cb2713b4f34146b533d0c71
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-11-12 04:50:06 +0100

    .config/nvim/MAYBES.md
    Add links for a YT video I want to try

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -536,6 +536,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         ruff = {},
+        markdownlint = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -629,6 +630,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        markdown = { 'markdownlint' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
