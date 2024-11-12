ac723ae7c9d88d4e23327f0b8222644bb549a6a5
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-11-12 01:00:06 +0100

    script: adjust qtile install - needs sorting

--- .debian-scripts/qtile.sh
+++ .debian-scripts/qtile.sh
@@ -1,4 +1,6 @@
-sudo apt install python3-venv xinit libglib2.0-bin build-essential libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libpython3-dev libiw-dev feh picom x11-utils fonts-recommended kitty dex
+sudo apt install python3-venv xinit libglib2.0-bin build-essential libpangocairo-1.0-0 libgdk-pixbuf2.0-0 \
+	libpython3-dev libiw-dev feh picom x11-utils fonts-recommended kitty dex python3-pip python3-dbus-next python3-cffi \
+	python3-cairocffi libxcb-render0-dev libffi-dev python3-cffi
 sudo python3 -m venv /opt/qtile --upgrade-deps
 sudo /opt/qtile/bin/pip install --upgrade qtile[widgets] qtile-extras
 cat <<EOF> $HOME/example.xinitrc
@@ -8,5 +10,3 @@ cat <<EOF> $HOME/example.xinitrc
 
 exec /opt/qtile/bin/qtile start
 EOF
-sudo apt install libxcb-render0-dev
-sudo apt install libffi-dev
