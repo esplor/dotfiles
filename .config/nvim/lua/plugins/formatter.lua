d048c18fcdedb7db64c6e3dea31d77d3570b393d
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-11-14 00:59:39 +0100

    linters treesitter and lualine

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -33,7 +33,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
-      python = { 'black' },
+      python = { 'ruff', 'black' },
       markdown = { 'markdownlint' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
