ae46f984c580525570ad4c376c2c129a3fb17724
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-11-14 22:42:19 +0100

    script: Add psutils as a qtile dependency

--- .debian-scripts/qtile.sh
+++ .debian-scripts/qtile.sh
@@ -1,6 +1,6 @@
 sudo apt install python3-venv xinit libglib2.0-bin build-essential libpangocairo-1.0-0 libgdk-pixbuf2.0-0 \
 	libpython3-dev libiw-dev feh picom x11-utils fonts-recommended kitty dex python3-pip python3-dbus-next python3-cffi \
-	python3-cairocffi libxcb-render0-dev libffi-dev python3-cffi
+	python3-cairocffi libxcb-render0-dev libffi-dev python3-cffi psutils
 sudo python3 -m venv /opt/qtile --upgrade-deps
 sudo /opt/qtile/bin/pip install --upgrade qtile[widgets] qtile-extras
 cat <<EOF> $HOME/example.xinitrc
