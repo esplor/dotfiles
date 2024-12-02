529c67afb804221a8915adffe58d1b63cc0a02d3
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-12-02 18:32:24 +0100

    script: nvim build clone

---
+++ .debian-scripts/nvim-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+NVIM_BUILD_DIR=/tmp/neovim-git/
+# https://github.com/neovim/neovim/blob/master/BUILD.md
+# The build type determines the level of used compiler optimizations and debug information:
+# ---
+# Release: Full compiler optimizations and no debug information. Expect the best performance from this build type.
+# Often used by package maintainers.
+# ---
+# Debug: Full debug information; few optimizations. Use this for development to get meaningful output 
+# from debuggers like GDB or LLDB. This is the default if CMAKE_BUILD_TYPE is not specified.
+# ---
+# RelWithDebInfo ("Release With Debug Info"): Enables many optimizations and adds enough debug info so that
+# when Neovim ever crashes, you can still get a backtrace.
+
+[ -d $NVIM_BUILD_DIR ] && rm -rf $NVIM_BUILD_DIR # If clone dir exist, delete before cloning
+
+sudo apt-get update \
+	&& sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential \
+	&& git clone --depth=1 --branch=stable https://github.com/neovim/neovim /tmp/neovim-git \
+	&& cd /tmp/neovim-git && make CMAKE_BUILD_TYPE=Release && sudo make install
