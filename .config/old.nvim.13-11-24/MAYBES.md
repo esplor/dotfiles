2632ec41456ba1519a5a5270d1b4448823d579f0
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-12-08 18:50:39 +0100

    Remove old nvim configs

--- .config/old.nvim.13-11-24/MAYBES.md
+++
@@ -1,4 +0,0 @@
-# Maybe setup nvim as this
-
-- [YT-Video - chris@machine](https://www.youtube.com/watch?v=KGJV0n70Mxs)
-- [GH-Lunarvim](https://github.com/LunarVim/Launch.nvim)
--- .config/old.nvim/lua/esplor/init.lua
+++
@@ -1,9 +0,0 @@
-require("esplor.plugins") 
-require("esplor.remap")
-
--- Declare opt as vim.opt
-local opt = vim.opt
-
-opt.number = true
-opt.relativenumber = true
---let g:loaded_perl_provider = 0
