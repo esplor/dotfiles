239ad3a88081c67bd9f46d890f2315e02b4d8299
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-05-29 02:41:01 +0200

    use based.. instead of pylsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -167,7 +167,8 @@ return {
       -- ruff = {},
       -- markdownlint = {},
       -- black = {},
-      pylsp = {},
+      -- pylsp = {},
+      basedpyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
