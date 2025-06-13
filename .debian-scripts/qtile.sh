1fde1c004566b1344c142356d75979f8ddd1bfd6
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-06-14 00:35:24 +0200

    qtile: Remove qtile extras

--- .debian-scripts/qtile.sh
+++ .debian-scripts/qtile.sh
@@ -1,12 +1,4 @@
-sudo apt install python3-venv xinit libglib2.0-bin build-essential libpangocairo-1.0-0 libgdk-pixbuf2.0-0 \
-	libpython3-dev libiw-dev feh picom x11-utils fonts-recommended kitty dex python3-pip python3-dbus-next python3-cffi \
-	python3-cairocffi libxcb-render0-dev libffi-dev python3-cffi psutils
-sudo python3 -m venv /opt/qtile --upgrade-deps
-sudo /opt/qtile/bin/pip install --upgrade qtile[widgets] qtile-extras pywal16
-#cat <<EOF> $HOME/example.xinitrc
-# Rename file to .xinitrc
-# something cool & # Remember "and" at the end.
-# Have a look at xprofile
+sudo apt install libpython3-dev python3-venv xinit build-essential picom x11-utils fonts-recommended kitty psmisc imagemagick pipewire-pulse pavucontrol upower feh rofi gnome-themes-extra
 
-#exec /opt/qtile/bin/qtile start
-#EOF
+sudo python3 -m venv /opt/qtile --upgrade-deps
+sudo /opt/qtile/bin/pip install --upgrade qtile[widgets] pywal16
