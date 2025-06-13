111f4deb380246e1f4228e3d845356ff13dd8e5d
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-06-14 00:32:01 +0200

    Cleanup google chrome install

--- .debian-scripts/browser-google-chrome.sh
+++ .debian-scripts/browser-google-chrome.sh
@@ -2,7 +2,7 @@
 
 #TODO: Something is creating a -, when running the script
 
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee - /etc/apt/trusted.gpg.d/google-key.pub
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google-key.pub
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/google-key.pub] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update && sudo apt install -y google-chrome-stable
 
