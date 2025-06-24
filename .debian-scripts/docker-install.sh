469ccd52c9ce1109f6907364da9bbe89a6d8c387
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-06-24 12:07:23 +0200

    docker: Remove for loop

--- .debian-scripts/docker-install.sh
+++ .debian-scripts/docker-install.sh
@@ -1,4 +1,4 @@
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt purge docker.io docker-doc docker-compose podman-docker containerd runc --auto-remove
 
 # Add Docker's official GPG key:
 sudo apt-get update
@@ -9,8 +9,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
