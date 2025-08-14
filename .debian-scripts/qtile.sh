f91418b1bcf1f89e7a5cf8d6a8c6bf89da9821a3
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-08-15 01:30:01 +0200

    libglib required for gsettings command

--- .debian-scripts/qtile.sh
+++ .debian-scripts/qtile.sh
@@ -1,4 +1,4 @@
-sudo apt install libpython3-dev python3-venv libiw-dev xinit build-essential picom x11-utils fonts-recommended kitty psmisc imagemagick pipewire-pulse pavucontrol upower feh rofi gnome-themes-extra
+sudo apt install libpython3-dev python3-venv libiw-dev libglib2.0-bin xinit build-essential picom x11-utils fonts-recommended kitty psmisc imagemagick pipewire-pulse pavucontrol upower feh rofi gnome-themes-extra
 
 sudo python3 -m venv /opt/qtile --upgrade-deps
 sudo /opt/qtile/bin/pip install --upgrade qtile[widgets] pywal16
