2749fc13a37e30f86779747d4b83c902c82c24c2
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-08-15 02:02:26 +0200

    extract only ttf to /usr/local ..

--- .debian-scripts/nerd-fonts.sh
+++ .debian-scripts/nerd-fonts.sh
@@ -3,7 +3,7 @@
 . ./functions
 
 #NERD_DIR=$HOME/.fonts/nerdfonts
-NERD_DIR=/usr/share/fonts/nerd-fonts
+NERD_DIR=/usr/local/share/fonts/nerd-fonts
 
 [ -d $NERD_DIR ] || sudo mkdir -p $NERD_DIR
 
@@ -15,7 +15,7 @@ fonts=(
 
 for font in ${fonts[*]} 
 do
-    url_stdout https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.tar.xz | sudo tar -xJf - -C $NERD_DIR
+    url_stdout https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.tar.xz | sudo tar -xJf - -C $NERD_DIR --wildcards "*.ttf"
     print_dots
     echo $font unpacked to $NERD_DIR
 done
