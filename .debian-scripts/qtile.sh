e24f62c1d1a60d56c9baeb27d84522ae46fb842c
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-08-18 02:03:52 +0200

    Cleaner. Add xdg-desktop-portal-gtk

--- .debian-scripts/qtile.sh
+++ .debian-scripts/qtile.sh
@@ -1,4 +1,9 @@
-sudo apt install libpython3-dev python3-venv libiw-dev libglib2.0-bin xinit build-essential picom x11-utils fonts-recommended kitty psmisc imagemagick pipewire-pulse pavucontrol upower feh rofi gnome-themes-extra
+sudo apt install \
+	libpython3-dev python3-venv \
+	libiw-dev libglib2.0-bin xinit build-essential picom \
+	x11-utils xdg-desktop-portal-gtk \
+	fonts-recommended kitty psmisc imagemagick \
+	pipewire-pulse pavucontrol upower feh rofi gnome-themes-extra
 
 sudo python3 -m venv /opt/qtile --upgrade-deps
 sudo /opt/qtile/bin/pip install --upgrade qtile[widgets] pywal16
