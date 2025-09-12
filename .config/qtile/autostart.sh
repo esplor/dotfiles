2172bd491c2144ba7b1d255da1514017937eb7a2
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-09-12 16:26:56 +0200

    Remove whitespace

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh 
+#!/usr/bin/env sh
 dex -a
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 gnome-keyring-daemon -s
-/opt/qtile/bin/wal -i /home/eslo/Wallpapers/ --recursive -a 75 -b 000000
+$HOME/.venv/bin/wal -i /home/eslo/Wallpapers/ --recursive -a 75 -b 000000
