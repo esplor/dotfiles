542e190a28af21e5e3da2581a26459a2aa4d1690
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2025-09-12 16:56:10 +0200

    Add mpv as dependency

--- .debian-scripts/qtile.sh
+++ .debian-scripts/qtile.sh
@@ -1,7 +1,7 @@
 sudo apt install \
 	bibata-cursor-theme libpython3-dev python3-venv \
 	libiw-dev libglib2.0-bin xinit build-essential picom \
-	x11-utils xdg-desktop-portal-gtk \
+	x11-utils xdg-desktop-portal xdg-desktop-portal-gtk mpv \
 	fonts-recommended kitty psmisc imagemagick \
 	pipewire-pulse pavucontrol upower feh rofi gnome-themes-extra easyeffects
 
